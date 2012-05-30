8ed4e91de5e1a95cbb6b7f8831334dd45b1e243e
sov-87 <afetisov87@gmail.com>
2012-05-30 15:07:00 +0400

    DEV-14838

--- app/controllers/buyers_route_controller.rb
+++ app/controllers/buyers_route_controller.rb
@@ -1,63 +1,59 @@
+# encoding: utf-8
+
 class BuyersRouteController < ApplicationController
   
   def index
 	set_conditions
-	rst_site = ActiveRecord::Base.connection.select_all( "select id, name from site where istarget = 1 " )
-	@site_list = rst_site.collect {|p| [ p["name"], p["id"] ] }
+	@rst_site = ActiveRecord::Base.connection.select_all( "select id, name from site where istarget = 1 " )
+	@site_list = @rst_site.collect {|p| [ p["name"], p["id"] ] }
 
-	rst_spv = ActiveRecord::Base.connection.select_all( " select id, name from sp_values where sp_tp = 1284 " )
-	@spv_list = rst_spv.collect {|p| [ p["name"], p["id"] ] }
+	@rst_spv = ActiveRecord::Base.connection.select_all( " select id, name from sp_values where sp_tp = 1284 " )
+	@spv_list = @rst_spv.collect {|p| [ p["name"], p["id"] ] }
 
-	rst_route = ActiveRecord::Base.connection.select_all(
-	" select id, name, points from buyers_route where site = #{@site} and spv_id = #{@spv_id} order by name " )
+	@rst_route = Proxycat.connection.select_all("exec buyers_route_get_route
+		#{@site},
+		#{@spv_id}")
+	
 	@route_list = []
 	@route_index = 0
 	@route_id = session[:route_id].to_i 
 	@route_id = 0 if not @route_id
-	rst_route.each_with_index do |p,i| 
+	@rst_route.each_with_index do |p,i| 
 		@route_list << [ p["name"], i ]
 		@route_index = i if @route_id == p["id"]
 	end
-	if rst_route.size > 0
-		@route_id = rst_route[@route_index]["id"]
-		@route_points = rst_route[@route_index]["points"]
+	if @rst_route.size > 0
+		@route_id = @rst_route[@route_index]["id"]
+		@route_points = @rst_route[@route_index]["points"]
 	else
 		@route_id = 0
 		@route_points = ""
 	end 
-	@route_json = rst_route.to_json( :only => [ "id", "points", "name" ] )
-	rst_buyers = ActiveRecord::Base.connection.select_all( " select distinct g.id id,   
-																	 g.latitude latitude, g.longitude longitude
-															from placeunload g  
-															 ") 
+	@route_json = @rst_route.to_json( :only => [ "id", "points", "name" ] )
+	@rst_buyers = Proxycat.connection.select_all("exec buyers_route_get_buyers") 
 															
 	@rst_new = @rst_buyers.to_json( :only => [ "id", "longitude", "latitude" ] ) 
   end
   
   def save_point
 	if params[:a]
-		@route_id = params[:a][:routeid]
-		session[:route_id] = @route_id 
-		r = BuyersRoute.find( @route_id )
-		r.points = params[:a][:points]
-		r.save
-		
+		route_id = params[:a][:routeid]
+		session[:route_id] = route_id
+
 		single_strings=params[:a][:points_str].split(';')
 		query_insert_points=""
 		single_strings.each_with_index do |single_string|
 			query_insert_points+="INSERT INTO geozone_coordinates (id, buyers_route, number, longitude, latitude) VALUES(idgenerator('geozone_coordinates'), #{@route_id}, #{single_string});"
 		end
 		
-		ActiveRecord::Base.connection.execute("
-		DELETE FROM geozone_coordinates WHERE buyers_route=#{@route_id};
-		#{query_insert_points}")
+		Proxycat.connection.execute("exec buyers_route_save_point
+			#{route_id},
+			'#{params[:a][:points]}',
+			'#{query_insert_points}'")
 	end
 	redirect_to :action => "index"
   end
 
-
-
-  
 private
 
   def set_conditions
