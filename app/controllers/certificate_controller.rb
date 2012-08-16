02f9aa047cd30f474e477067064a43e71b9a7caa
sov-87 <afetisov87@gmail.com>
2012-08-17 03:45:31 +0400

    DEV-15724

---
+++ app/controllers/certificate_controller.rb
@@ -0,0 +1,107 @@
+# encoding: utf-8
+
+require 'base64'
+
+class CertificateController < ApplicationController
+
+  def index
+	logger.info "???"
+  end
+  
+  #получение информации о сертификатах
+  def certificate_info
+	@inn=params[:info_inn]
+	@ndoc=params[:info_ndoc]
+	@goods_name=params[:info_goods_name]
+	
+	logger.info "SELECT
+			c.id,
+			g.short_name goods_name
+		FROM
+			sale_order so
+			JOIN sordgoods sog ON sog.id=so.id
+			JOIN cert_goods cg ON sog.goods = cg.goods
+			join certificate c on c.id = cg.certificate
+			join cert_center cc on c.center = cc.id
+			JOIN goods g ON g.id=sog.goods
+			JOIN buyers b ON b.id=sog.client
+			JOIN payers p ON b.payer=p.id OR p.partner=b.partner
+		WHERE
+			(p.inn=#{ActiveRecord::Base.connection.quote(@inn)} OR p.inn IS NULL)
+			AND
+			so.ndoc=#{ActiveRecord::Base.connection.quote(@ndoc)}
+			AND
+			g.short_name like '%'+#{ActiveRecord::Base.connection.quote(@goods_name)}+'%'
+			AND
+			cg.status=1
+			AND
+			so.ddate BETWEEN c.ddateb AND c.ddatee"
+	begin
+		@res=ActiveRecord::Base.connection.select_all("SELECT
+			c.id,
+			g.short_name goods_name
+		FROM
+			sale_order so
+			JOIN sordgoods sog ON sog.id=so.id
+			JOIN cert_goods cg ON sog.goods = cg.goods
+			join certificate c on c.id = cg.certificate
+			JOIN goods g ON g.id=sog.goods
+			JOIN buyers b ON b.id=sog.client
+			JOIN payers p ON b.payer=p.id OR p.partner=b.partner
+		WHERE
+			p.inn=#{ActiveRecord::Base.connection.quote(@inn)}
+			AND
+			so.ndoc=#{ActiveRecord::Base.connection.quote(@ndoc)}
+			AND
+			g.short_name like '%'+#{ActiveRecord::Base.connection.quote(@goods_name)}+'%'
+			AND
+			cg.status=1
+			AND
+			so.ddate BETWEEN c.ddateb AND c.ddatee")
+	rescue => t
+		logger.info t
+	end
+	
+	render :partial => 'certificate'
+  end
+  
+  def get_certificate
+	id=params[:id]
+	inn=params[:inn]
+	ndoc=params[:ndoc]
+	res=nil
+	begin
+		res=ActiveRecord::Base.connection.select_one("
+		SELECT
+			picture.picture
+		FROM
+			sale_order so
+			JOIN sordgoods sog ON sog.id=so.id
+			JOIN cert_goods cg ON sog.goods = cg.goods
+			join certificate c on c.id = cg.certificate
+			JOIN cert_image ci ON ci.certificate=c.id
+			JOIN picture ON picture.id=ci.picture
+			JOIN buyers b ON b.id=sog.client
+			JOIN payers p ON b.payer=p.id OR p.partner=b.partner
+		WHERE
+			(p.inn=#{ActiveRecord::Base.connection.quote(inn)} OR p.inn IS NULL)
+			AND
+			so.ndoc=#{ActiveRecord::Base.connection.quote(ndoc)}
+			AND
+			c.id=#{ActiveRecord::Base.connection.quote(id)}
+			AND
+			cg.status=1
+			AND
+			ci.status1=1
+			AND
+			so.ddate BETWEEN c.ddateb AND c.ddatee")
+	rescue => t
+		logger.info t
+	end
+	
+	response.headers['Content-Type'] = 'image/jpeg'
+	response.headers['Content-Disposition'] = 'inline'
+
+	render :text => (!res.nil?) ? res['picture'] : ''
+  end
+end
