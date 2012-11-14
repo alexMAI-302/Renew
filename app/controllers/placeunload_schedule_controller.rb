c3073f067d80486b021da385408822ddcb9a0fbd
sov-87 <afetisov87@gmail.com>
2012-11-14 12:09:50 +0400

    DEV-16368

--- app/controllers/placeunload_schedule_controller.rb
+++ app/controllers/placeunload_schedule_controller.rb
@@ -2,30 +2,33 @@
 class PlaceunloadScheduleController < ApplicationSimpleErrorController
   def index
   end
-  
+
   def get
-	res=Proxycat.connection.select_all("exec dbo.placeunload_schedule_get #{params[:salesman_id].to_i}")
-			
-	render :text => res.to_json
+    res=Proxycat.connection.select_all("
+    exec dbo.placeunload_schedule_get
+    #{params[:salesman_id].to_i},
+    '#{Time.parse(params[:ddate]).strftime('%F')}'")
+
+    render :text => res.to_json
   end
-  
+
   def save
-	Proxycat.connection.execute("
-	exec dbo.placeunload_schedule_save
-		#{params[:id].to_i},
-		#{Proxycat.connection.quote(params[:monday])},
-		#{Proxycat.connection.quote(params[:tuesday])},
-		#{Proxycat.connection.quote(params[:wednesday])},
-		#{Proxycat.connection.quote(params[:thursday])},
-		#{Proxycat.connection.quote(params[:friday])},
-		#{params[:site_id].to_i}")
-			
-		render :text => "[]"
+    day_of_week=params[:day_of_week].to_i
+    if day_of_week>=1 && day_of_week<=5
+      Proxycat.connection.execute("
+      	exec dbo.placeunload_schedule_save
+      		#{params[:id].to_i},
+      		#{params[:day_of_week].to_i},
+      		'#{Time.parse(params[:ddate]).strftime('%F')}',
+      		#{params[:site_id].to_i}")
+    end
+
+    render :text => "[]"
   end
-  
+
   def get_salesman
-	res = Proxycat.connection.select_all("exec placeunload_schedule_get_salesman '#{Proxycat.connection.quote_string(params[:query])}'")
-	
-	render :text => res.to_json
+    res = Proxycat.connection.select_all("exec placeunload_schedule_get_salesman '#{Proxycat.connection.quote_string(params[:query])}'")
+
+    render :text => res.to_json
   end
 end
--- public/javascripts/app/controller/placeunloadSchedule.js
+++ public/javascripts/app/controller/placeunloadSchedule.js
@@ -14,85 +14,118 @@ Ext.define('app.controller.placeunloadSchedule', {
 		function showServerError(response, options) {
 			Ext.Msg.alert('Ошибка', response.responseText);
 			mainContainer.setLoading(false);
-		}
+		};
 	
 		function loadSchedule(){
 			schedulePanel.setLoading(true);
 			scheduleStore.proxy.extraParams={
-				salesman_id: salesmansCombo.value
+				salesman_id: Ext.getCmp('salesmanCombo').getValue(),
+				ddate: Ext.getCmp('ddate').getValue(),
 			};
 			scheduleStore.load();
-		}
+		};
+		
+		function selectDayOfWeek(dayIndex, rowIndex){
+			var r=schedulePanel.getStore().getAt(rowIndex);
+			r.set("day_of_week", dayIndex);
+			r.set("monday", false);
+			r.set("tuesday", false);
+			r.set("wednesday", false);
+			r.set("thursday", false);
+			r.set("friday", false);
+			switch(dayIndex){
+				case 1:
+					r.set("monday", true);
+				break;
+				case 2:
+					r.set("tuesday", true);
+				break;
+				case 3:
+					r.set("wednesday", true);
+				break;
+				case 4:
+					r.set("thursday", true);
+				break;
+				case 5:
+					r.set("friday", true);
+				break;
+			}
+		};
 	
 		var salesmansStore = Ext.create('Ext.data.Store', {
-			model: 'app.model.valueModel',
-			proxy: {
-				type: 'rest',
-				url : '/placeunload_schedule/get_salesman',
-				reader: {
-					type: 'json'
-				}
-			}
-		});
-		
-		var scheduleStore = Ext.create('Ext.data.Store', {
-			model: 'app.model.placeunloadSchedule.scheduleModel',
-			proxy: {
-				type: 'rest',
-				api: {
-					update: '/placeunload_schedule/save'
-				},
-				url : '/placeunload_schedule/get',
-				reader: {
-					type: 'json'
-				}
-			},
-			listeners: {
-				"load": function(store, records, successful, operation, options ){
-					if(successful){
-						schedulePanel.setLoading(false);
+				model: 'app.model.valueModel',
+				proxy: {
+					type: 'rest',
+					url : '/placeunload_schedule/get_salesman',
+					reader: {
+						type: 'json'
 					}
 				}
-			}
-		});
-		
-		var filterPanel=Ext.create('Ext.form.Panel',{
-			layout: {
-				type: 'hbox'
-			},
-			defaults: {
-				style: {
-					margin: '5px'
-				}
-			}
-		});
-		
-		var salesmansCombo=Ext.create('Ext.form.ComboBox', {
-			fieldLabel: 'Торговый представитель',
-			store: salesmansStore,
-			displayField: 'name',
-			valueField: 'id',
-			allowBlank: false,
-			labelWidth: 150,
-			width: 400,
-			listeners: {
-				"select": function(field, value, options ) {
-					filterSchedule.setDisabled(value[0].id==null);
-					return true;
+			}),
+				scheduleStore = Ext.create('Ext.data.Store', {
+				model: 'app.model.placeunloadSchedule.scheduleModel',
+				proxy: {
+					type: 'rest',
+					api: {
+						update: '/placeunload_schedule/save'
+					},
+					url : '/placeunload_schedule/get',
+					reader: {
+						type: 'json'
+					}
 				},
-				"change": function(field, newValue, oldValue, options) {
-					filterSchedule.setDisabled(newValue==null);
-					return true;
+				listeners: {
+					"load": function(store, records, successful, operation, options ){
+						if(successful){
+							schedulePanel.setLoading(false);
+						}
+					}
 				}
-			}
-		});
+			}),
+			filterPanel=Ext.create('Ext.form.Panel',{
+				layout: {
+					type: 'hbox'
+				},
+				defaults: {
+					style: {
+						margin: '5px'
+					}
+				},
+				items: [{
+					id: 'salesmanCombo',
+					xtype: 'combobox',
+					fieldLabel: 'Торговый представитель',
+					store: salesmansStore,
+					displayField: 'name',
+					valueField: 'id',
+					allowBlank: false,
+					labelWidth: 150,
+					width: 400,
+					listeners: {
+						"select": function(field, value, options ) {
+							filterSchedule.setDisabled(value[0].id==null);
+							return true;
+						},
+						"change": function(field, newValue, oldValue, options) {
+							filterSchedule.setDisabled(newValue==null);
+							return true;
+						}
+					}
+				},{
+					id: 'ddate',
+					xtype: 'datefield',
+					fieldLabel: 'Конец периода',
+					format: 'd.m.Y',
+					altFormat: 'd/m/Y|d m Y',
+					startDay: 1,
+					value: new Date(Ext.Date.now())
+				}]
+			}),
+			filterSchedule=Ext.create('Ext.Button', {
+				text    : 'Фильтр',
+				handler : loadSchedule
+			});
 		
-		var filterSchedule=Ext.create('Ext.Button', {
-			text    : 'Фильтр',
-			handler : loadSchedule
-		});
-		
-		filterPanel.add(salesmansCombo);
 		filterPanel.add(filterSchedule);
 		
 		var cellEditing = Ext.create('Ext.grid.plugin.CellEditing', {
@@ -123,42 +156,74 @@ Ext.define('app.controller.placeunloadSchedule', {
 					header: 'Понедельник',
 					align: 'center',
 					dataIndex: 'monday',
-					xtype: 'checkcolumn'
+					xtype: 'checkcolumn',
+					listeners: {
+						checkchange: function(checkColumn, rowIndex, checked, eOpts){
+							selectDayOfWeek(1, rowIndex);
+							return true;
+						}
+					}
 				},
 				{
 					width: 70,
 					header: 'Вторник',
 					align: 'center',
 					dataIndex: 'tuesday',
-					xtype: 'checkcolumn'
+					xtype: 'checkcolumn',
+					listeners: {
+						checkchange: function(checkColumn, rowIndex, checked, eOpts){
+							selectDayOfWeek(2, rowIndex);
+							return true;
+						}
+					}
 				},
 				{
 					width: 70,
 					header: 'Среда',
 					align: 'center',
 					dataIndex: 'wednesday',
-					xtype: 'checkcolumn'
+					xtype: 'checkcolumn',
+					listeners: {
+						checkchange: function(checkColumn, rowIndex, checked, eOpts){
+							selectDayOfWeek(3, rowIndex);
+							return true;
+						}
+					}
 				},
 				{
 					width: 70,
 					header: 'Четверг',
 					align: 'center',
 					dataIndex: 'thursday',
-					xtype: 'checkcolumn'
+					xtype: 'checkcolumn',
+					listeners: {
+						checkchange: function(checkColumn, rowIndex, checked, eOpts){
+							selectDayOfWeek(4, rowIndex);
+							return true;
+						}
+					}
 				},
 				{
 					width: 70,
 					header: 'Пятница',
 					align: 'center',
 					dataIndex: 'friday',
-					xtype: 'checkcolumn'
+					xtype: 'checkcolumn',
+					listeners: {
+						checkchange: function(checkColumn, rowIndex, checked, eOpts){
+							selectDayOfWeek(5, rowIndex);
+							return true;
+						}
+					}
 				}
 			],
 			tbar: [
 				{
 					text    : 'Сохранить',
 					handler : function() {
-						scheduleStore.proxy.extraParams=null;
+						scheduleStore.proxy.extraParams={
+							ddate: Ext.getCmp('ddate').getValue(),
+						};
 						scheduleStore.sync(function(){
 							loadSchedule();
 						});
