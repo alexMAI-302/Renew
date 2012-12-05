313ba9a70197cccab4d8788a785dab0b571e1b50
sov-87 <afetisov87@gmail.com>
2012-12-05 18:37:19 +0400

    DEV-16724

--- public/javascripts/app/model/AutoTransport/NomenclatureModel.js
+++ public/javascripts/app/model/AutoTransport/NomenclatureModel.js
@@ -3,7 +3,7 @@ Ext.define('app.model.AutoTransport.NomenclatureModel', {
 	fields: [
 	{name: 'id'			, type:'int'},
 	{name: 'name'		, type:'string'},
-	{name: 'at_ggroup'	, type:'int'},
-	{name: 'measure'	, type:'int'},
+	{name: 'at_ggroup'	, type:'int', useNull: true},
+	{name: 'measure'	, type:'int', useNull: true},
 	{name: 'cnt'		, type:'int'}]
 });
