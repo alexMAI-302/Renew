b39a8e6bcff034de28aa1fc08e9f931cf2a69dbb
sov-87 <afetisov87@gmail.com>
2013-01-31 18:57:20 +0400

    PRO-1027

--- app/controllers/certificate_controller.rb
+++ app/controllers/certificate_controller.rb
@@ -25,14 +25,14 @@ class CertificateController < ApplicationPageErrorController
     }
     
     certificates={}
+    goods={}
     to_render=[]
     
-    res=ActiveRecord::Base.connection.select_all("
+    info=ActiveRecord::Base.connection.select_all("
 		SELECT
 			certificate_id,
 			certificate_number,
 			picture_id,
-			ts,
 			ndoc,
 			goods_code,
 			goods_name
@@ -40,50 +40,73 @@ class CertificateController < ApplicationPageErrorController
 			renew_web.certificates_get(#{ActiveRecord::Base.connection.quote(inn)},
 			#{ActiveRecord::Base.connection.quote(ndoc)},
 			#{ActiveRecord::Base.connection.quote(goods_code)})")
-
-    if !res.nil? then
-      res.each do |picture|
-        actual=false
-        file_name = "#{RAILS_ROOT}/public/images/certificates/#{picture['picture_id']}.jpg"
-        if File.exist?(file_name) then
-          if File.ctime(file_name) >= Time.parse(picture['ts']) then
-          actual=true
+	  if !info.nil? then
+	    #начать обновление кэша на диске
+	    picture_ids=info.collect do |i|
+        i["picture_id"]
+      end
+      
+      pictures_ts=RuzaPicture.connection.select_all("SELECT DISTINCT id, ts FROM picture WHERE id IN (#{picture_ids*','})")
+      
+      if !pictures_ts.nil? then
+        pictures_ts.each do |picture|
+          actual=false
+          file_name = "#{RAILS_ROOT}/public/images/certificates/#{picture['id']}.jpg"
+          if File.exist?(file_name) then
+            if File.ctime(file_name) >= Time.parse(picture['ts']) then
+              actual=true
+            end
           end
-        end
-
-        if !actual then
-          bin_picture=RuzaPicture.connection.select_value("SELECT picture FROM picture WHERE id=#{picture['picture_id']}")
-          file=File.new(file_name, 'wb')
-          file.write(bin_picture)
-          file.close
-          File.chmod(0644, file_name)
-        end
-
-        if certificates.has_key?(picture['certificate_id']) then
-          if !certificates[picture['certificate_id']]['picture_ids'].include?(picture['picture_id']) then
-            certificates[picture['certificate_id']]['picture_ids'] << picture['picture_id']
+          
+          if !actual then
+            bin_picture=RuzaPicture.connection.select_value("SELECT picture FROM picture WHERE id=#{picture['id']}")
+            file=File.new(file_name, 'wb')
+            file.write(bin_picture)
+            file.close
+            File.chmod(0644, file_name)
           end
-        else
-          certificates[picture['certificate_id']] = {
-            'picture_ids' => [picture['picture_id']],
-            'certificate_number' => picture['certificate_number'],
-            'ndoc' => picture['ndoc'],
-            'goods_code' => picture['goods_code'],
-            'goods_name' => picture['goods_name']}
         end
       end
-    end
-    
+      #Закончить обновление кэша на диске
+      
+      info.each do |i|
+        
+        if goods.has_key?(i['certificate_id']) then
+          if !goods[i['certificate_id']]['goods_codes'].include?(i['goods_code']) then
+            goods[i['certificate_id']]['goods_codes'] << i['goods_code']
+            goods[i['certificate_id']]['goods_names'] << i['goods_name']
+          end
+        else
+          goods[i['certificate_id']]={}
+          goods[i['certificate_id']]['goods_codes'] = [i['goods_code']]
+          goods[i['certificate_id']]['goods_names'] = [i['goods_name']]
+        end
+        
+        if certificates.has_key?(i['certificate_id']) then
+          if !certificates[i['certificate_id']]['picture_ids'].include?(i['picture_id']) then
+            certificates[i['certificate_id']]['picture_ids'] << i['picture_id']
+          end
+        else
+          certificates[i['certificate_id']] = {
+            'picture_ids' => [i['picture_id']],
+            'certificate_number' => i['certificate_number'],
+            'ndoc' => i['ndoc']}
+        end
+      end
+	  end
+	  
     certificates.each do |id, cert|
-      to_render << {
-        'id' => id,
-        'ndoc' => cert['ndoc'],
-        'goods_code' => cert['goods_code'],
-        'goods_name' => cert['goods_name'],
-        'certificate_number' => cert['certificate_number'],
-        'jpg_url' => "/certificate/certificate_pages?pictures=#{cert['picture_ids'].join(',')}&certificate_number=#{cert['certificate_number']}&goods_name=#{cert['goods_name']}",
-        'pdf_url' => "/certificate/certificate_pdf?pictures=#{cert['picture_ids'].join(',')}&certificate_number=#{cert['certificate_number']}&goods_name=#{cert['goods_name']}"
-      }
+      goods[id]['goods_codes'].each_index do |goods_num|
+        to_render << {
+          'id' => "#{id}_#{goods[id]['goods_codes'][goods_num]}",
+          'ndoc' => cert['ndoc'],
+          'goods_code' => goods[id]['goods_codes'][goods_num],
+          'goods_name' => goods[id]['goods_names'][goods_num],
+          'certificate_number' => cert['certificate_number'],
+          'jpg_url' => "/certificate/certificate_pages?pictures=#{cert['picture_ids'].join(',')}&certificate_number=#{cert['certificate_number']}&goods_name=#{goods[id]['goods_names'][goods_num]}",
+          'pdf_url' => "/certificate/certificate_pdf?pictures=#{cert['picture_ids'].join(',')}&certificate_number=#{cert['certificate_number']}&goods_name=#{goods[id]['goods_names'][goods_num]}"
+        }
+      end
     end
 
     render :text => to_render.to_json
