0b98aee175e64c3edc18f0be39995fa3f8892400
sov-87 <afetisov87@gmail.com>
2013-04-08 16:46:19 +0400

    DEV-18258

--- public/javascripts/app/view/SalesmanRoutes/Container.js
+++ public/javascripts/app/view/SalesmanRoutes/Container.js
@@ -1,41 +1,37 @@
 Ext.define('app.view.SalesmanRoutes.Container', {
-    extend: 'Ext.container.Container',
+    extend: 'Ext.panel.Panel',
 	
 	requires: [
 		'app.view.SalesmanRoutes.Filter',
 		'app.view.SalesmanRoutes.Grid'
 	],
-
-    layout: {
-		type: 'fit'
-	},
 	
 	height: 600,
 	id: 'mainContainer',
 	renderTo: Ext.get('salesman_routes_js'),
 	
+	layout: {
+		type: 'border'
+	},
 	items: [
 		{
-			xtype: 'panel',
-			layout: {
-				type: 'border'
-			},
-			items: [
-				{
-					xtype: 'Filter',
-					region: 'north'
-				},
-				{
-					xtype: 'dataGrid',
-					region: 'west',
-					split: true
-				},
-				{
-					id: 'salesman_routes_map',
-					xtype: 'container',
-					region: 'center'
-				}
-			]
+			xtype: 'Filter',
+			region: 'north'
+		},
+		{
+			xtype: 'dataGrid',
+			region: 'west',
+			split: true
+		},
+		{
+			region: 'center',
+			height: '100%',
+			items:[{
+				width: '100%',
+				height: '100%',
+				id: 'salesman_routes_map',
+				xtype: 'container'
+			}]
 		}
-    ]
+	]
 });
