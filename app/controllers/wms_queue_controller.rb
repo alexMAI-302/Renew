c5d7f385072349858d3d3db503379de6e7212145
sov-87 <afetisov87@gmail.com>
2013-04-29 11:38:22 +0400

    DEV-18539

--- app/controllers/wms_queue_controller.rb
+++ app/controllers/wms_queue_controller.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 
-class WmsQueueController < ApplicationPageErrorController
+class WmsQueueController < ApplicationSimpleErrorController
   
   def wms_queue_entries
     method=request.method.to_s
