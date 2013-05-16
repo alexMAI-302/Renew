8d257d93d365b6fddd2a4e3f095372e64becde2c
sov-87 <afetisov87@gmail.com>
2013-05-16 11:02:31 +0400

    DEV-18620

--- public/javascripts/app/view/AutoTransport/Nomenclature/Grid.js
+++ public/javascripts/app/view/AutoTransport/Nomenclature/Grid.js
@@ -1,36 +1,57 @@
 Ext.define('app.view.AutoTransport.Nomenclature.Grid', {
-    extend: 'app.view.Lib.Grid.Panel',
+    extend: 'Ext.tree.Panel',
 	alias: 'widget.nomenclatureGroupsGrid',
 	
-	config: {
-		suffix: 'NomenclatureGroup',
-		disableSave: true,
-		disableRefresh: true,
-		disableDeleteColumn: true,
-		features: [
-			{
-				id: 'group',
-				ftype:'grouping',
-				groupHeaderTpl: '{name}'
-			}
-		],
-		title: 'Группы',
-		header: false,
-		columns: [
-			{
-				width: 200,
-				header: 'Наименование',
-				dataIndex: 'name',
-				field: {
-					xtype: 'textfield'
+	initComponent: function() {
+		var suffix = 'NomenclatureGroup';
+        Ext.apply(this, {
+			id: suffix+'Table',
+			rootVisible: false,
+			useArrows: true,
+			store: Ext.create('app.store.AutoTransport.Nomenclature.NomenclatureGroup', {}),
+			hideHeaders: true,
+			title: 'Группы',
+			header: false,
+			tbar: [
+				{
+					id : 'add'+suffix,
+					icon : '/ext/examples/shared/icons/fam/add.gif',
+					tooltip: 'Добавить'
+				},
+				{
+					id : 'delete'+suffix,
+					icon : '/ext/examples/shared/icons/fam/delete.gif',
+					disabled : true,
+					tooltip: 'Удалить'
 				}
-			},
-			{
-				width: 90,
-				header: 'Тип',
-				dataIndex: 'at_ggtype',
-				hidden: true
+			],
+			columns: [
+				{
+					width: 200,
+					xtype: 'treecolumn',
+					header: 'Наименование',
+					dataIndex: 'name',
+					editor: {
+						xtype: 'textfield'
+					}
+				}
+			],
+			plugins: [
+				{
+					ptype: 'cellediting',
+					clicksToEdit : 2,
+					pluginId: 'cellediting'+suffix
+				}
+			],
+			viewConfig: {
+				enableTextSelection: true,
+				plugins: {
+					ptype: 'treeviewdragdrop',
+					pluginId: suffix+'DnD'
+				}
 			}
-		]
-	}
+		});
+        
+        this.callParent(arguments);
+    }
 });
