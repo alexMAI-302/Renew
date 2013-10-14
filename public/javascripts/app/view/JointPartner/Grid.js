7a3606775f61b394414eab805fa1406d880cff7f
paul@unact.ru <paul@unact.ru>
2013-10-14 16:35:13 +0400

    DEV-19628

---
+++ public/javascripts/app/view/JointPartner/Grid.js
@@ -0,0 +1,48 @@
+Ext.define('app.view.JointPartner.Grid', {
+    extend: 'app.view.Lib.Grid.Panel',
+	alias: 'widget.jointPartnerGrid',
+	//requires: 'app.view.Lib.Grid.column.ComboColumn',
+	
+	config: {
+		store: 'JointParner.Partner',
+		suffix: 'Partner',
+		title: 'Партнёры',
+		store: 'JointPartner.Partner',
+		disableSave: true,
+		disableRefresh: false,
+		disableAdd: true,
+		disableDelete: true,
+		disableDeleteColumn: true,
+		disableAddColumn: true,
+		beforeButtons: [
+			{
+							id: 'filterInn',
+							xtype: 'textfield',
+							fieldLabel: 'ИНН',
+							width: 200,
+							labelWidth: 40
+					}
+				
+		],
+		columns: [
+			{
+				width: 200,
+				header: 'Партнёр',
+				dataIndex: 'name'
+			},
+			{
+				width: 200,
+				header: 'ИНН',
+				dataIndex: 'inn'
+			}
+		],
+		 addcol: function() {
+		   var newColumn = Ext.create('Ext.grid.column.Column', {
+		   header: 'new1',
+		   dataIndex: 'newdata',
+		   });
+		   this.headerCt.insert(this.columns.length, eColumn);
+		   this.getView().refresh();
+		 }
+	}
+});
