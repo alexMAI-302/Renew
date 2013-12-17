6f4399307ae5c30038ee9e827a0fa4446b64ad5e
Semukhin Maxim <m.semukhin@unact.ru>
2013-12-17 16:43:37 +0400

    DEV-21005

--- app/controllers/letter_controller.rb
+++ app/controllers/letter_controller.rb
@@ -11,8 +11,6 @@ class LetterController < ApplicationSimpleErrorController
       rst = ActiveRecord::Base.connection.select_all("
         call dbo.ask_konvert_term_period(#{params[:period].to_i}, #{prefix})")
       render :text => rst.to_json
-    #			when "post"
-    #			render :text => "[]"
     when "put"
       id = params[:id].to_i
       period = params[:period].to_i
@@ -21,7 +19,8 @@ class LetterController < ApplicationSimpleErrorController
       info = ActiveRecord::Base.connection.quote (params[:info])
       issued= params[:issued]? 1 : 0
       info_issued = ActiveRecord::Base.connection.quote (params[:info_issued])
-      #      user_issued = issued==1?session[:user_id] : 'null'
+      logger.info(0)
+
       if issue == 1
         id = ActiveRecord::Base.connection.select_value("
             BEGIN
@@ -75,17 +74,26 @@ class LetterController < ApplicationSimpleErrorController
 
               SELECT @id;
             END;")
+        logger.info(1)
         render :text=>{"success"=>true, "id" => id}.to_json
       elsif issue == 0 && ((info.nil? || info.strip =='') && (info_issued.nil? || info_issued.strip =='') && (issued == 0))
         ActiveRecord::Base.connection.update ("DELETE FROM dbo.term_konvert_period WHERE cterm=#{cterm} AND period=#{period};")
+        logger.info(2)
         info = ''
         render :text => {"success" => true, "info" => info, "issued" => issued, "info_issued" => info_issued}.to_json
       elsif issue == 0 && (!(info.nil? || info.strip =='') || !(info_issued.nil? || info_issued.strip =='') || (issued == 1))
-        ActiveRecord::Base.connection.update ("UPDATE dbo.term_konvert_period SET issue=#{issue}, info=#{info}, issued=#{issued}, info_issued=#{info_issued} WHERE  cterm=#{cterm} AND period=#{period};")
+        if id == 0
+          ActiveRecord::Base.connection.update ("
+            INSERT INTO dbo.term_konvert_period
+            (id, period, cterm, issue, info, issued, info_issued)
+            VALUES (idgenerator('dbo.term_konvert_period'), #{period}, #{cterm}, #{issue}, #{info}, #{issued}, #{info_issued});
+            ")
+        else
+          ActiveRecord::Base.connection.update ("UPDATE dbo.term_konvert_period SET issue=#{issue}, info=#{info}, issued=#{issued}, info_issued=#{info_issued} WHERE  cterm=#{cterm} AND period=#{period};")
+        end
+        logger.info(3)
         render :text => {"success" => true, "info" => info, "issued" => issued, "info_issued" => info_issued}.to_json
       end
-    #			when "delete"
-    #				render :text => "[]"
     end
   end
 
