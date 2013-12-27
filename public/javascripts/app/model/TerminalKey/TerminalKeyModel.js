44b84756817b7f035deb3c19b48cd9e6b1e9e1f0
Semukhin Maxim <m.semukhin@unact.ru>
2013-12-27 12:11:34 +0400

    DEV-20329

---
+++ public/javascripts/app/model/TerminalKey/TerminalKeyModel.js
@@ -0,0 +1,9 @@
+Ext.define('app.model.TerminalKey.TerminalKeyModel', {
+	extend: 'Ext.data.Model',
+	fields: [
+	{name: 'id'				, type: 'int'},
+	{name: 'name'				, type: 'string'},
+	{name: 'spv_id'				, type: 'int'},
+	{name: 'zoneid'				, type: 'int'}
+]
+});
---
+++ public/javascripts/app/view/TerminalKey/Container.js
@@ -0,0 +1,116 @@
+Ext.define('app.view.TerminalKey.Container', {
+	extend : 'Ext.panel.Panel',
+	alias : 'widget.terminalkeyPanel',
+
+	requires : ['app.view.TerminalKey.Filter', 'app.view.Lib.Grid.Panel', 'app.view.Lib.Grid.column.ComboColumn'],
+	renderTo: Ext.get('terminal_key_js'),
+	width : '100%',
+	layout : {
+		type : 'border'
+	},
+	height : Ext.getBody().getViewSize().height - 120,
+
+	items : [{
+		xtype : 'terminalkeyFilter',
+		region : 'north'
+	}, {
+		xtype : 'simpleGrid',
+		suffix : 'TerminalKey',
+		disableRefresh : true,
+		disableDeleteColumn : true,
+		disableDelete : true,
+		disableAdd : false,
+		region : 'center',
+		columns : [{
+			xtype : 'rownumberer',
+			width : 30
+		}, {
+			header : 'Идентификатор',
+			dataIndex : 'id',
+			hidden : true
+		}, 
+		{
+			width : 300,
+			header : 'Наименование',
+			dataIndex : 'name',
+			field : {
+				xtype : 'textfield'
+			}
+		}, 
+		{
+			width : 150,
+			header : 'Тип',
+			dataIndex : 'spv_id',
+			xtype : 'combocolumn',
+			store : 'app.store.TerminalKey.KeyTypeData'
+		}, 
+		{
+			width : 150,
+			header : 'Зона',
+			dataIndex : 'zoneid',
+			xtype : 'combocolumn',
+			store : 'app.store.TerminalKey.PpsZoneData'
+		}
+		]
+	},
+	{
+		xtype : 'simpleGrid',
+		suffix : 'TerminalKeyLog',
+		disableRefresh : true,
+		disableDeleteColumn : true,
+		disableDelete : true,
+		disableAdd : true,
+		disableSave : true,
+		region : 'south',
+		height : '30%', 
+		split: true,
+		columns : [{
+			xtype : 'rownumberer',
+			width : 30
+		}, {
+			header : 'Идентификатор',
+			dataIndex : 'id',
+			hidden : true
+		}, 
+		{
+			width : 150,
+			header : 'Операция',
+			dataIndex : 'type_op',
+			field : {
+				xtype : 'textfield'
+			}
+		}, 
+		{
+			width : 300,
+			header : 'Старое значение',
+			dataIndex : 'old_strvalue',
+			field : {
+				xtype : 'textfield'
+			}
+		}, 
+		{
+			width : 300,
+			header : 'Новое значение',
+			dataIndex : 'new_strvalue',
+			field : {
+				xtype : 'textfield'
+			}
+		}, {
+			width : 100,
+			header : 'Дата',
+			dataIndex : 'ts',
+			renderer : function(value, metaData, record) {
+				return (value) ? Ext.Date.format(new Date(value), 'd.m.Y H:i:s') : '';
+			}
+		}, 
+		{
+			width : 300,
+			header : 'Пользователь',
+			dataIndex : 'renew_user',
+			field : {
+				xtype : 'textfield'
+			}
+		}
+		]
+	}]
+}); 
