307d87ef91ace479a0215000fc9146a0ad67d468
VVarlamov <v.varlamov@unact.ru>
2014-01-23 15:07:39 +0400

    DEV-21086

---
+++ public/javascripts/app/view/RequestBudgets/Container.js
@@ -0,0 +1,104 @@
+Ext.define('app.view.RequestBudgets.Container', {
+    extend: 'app.view.Lib.Grid.Panel',
+
+	requires: [
+		'app.view.Lib.Grid.column.ComboColumn'
+	],
+
+	title: 'Заявки на бюджеты от производителей',
+	
+	renderTo: 'request_budgets_js',
+
+	height: Ext.getBody().getViewSize().height - 150,
+	
+	config:{
+		suffix: 'RequestBudgets',
+		store: 'RequestBudgets.RequestBudgets',
+		disableDelete: true,
+
+		
+		columns : [
+					{
+				xtype: 'datecolumn',
+				width: 100,
+				header: 'Дата',
+				dataIndex: 'ddate',
+				name: 'ddate',
+				format: 'd.m.Y',
+				field: {
+					xtype: 'datefield'
+				}
+			},
+			{
+				width : 150,
+				header : 'Сотрудник',
+				dataIndex : 'person',
+				xtype: 'combocolumn',
+				store: 'app.store.RequestBudgets.Person'
+			},
+			{
+				align: 'center',
+				width : 100,
+				header : 'Отдел',
+				dataIndex : 'emp_dept'
+			},
+			{
+				width : 150,
+				header : 'Партнер',
+				dataIndex : 'partner',
+				xtype: 'combocolumn',
+				store: 'app.store.RequestBudgets.Partners'
+			},
+			{
+				width : 150,
+				header : 'Канал NRC',
+				dataIndex : 'channel_NRC',
+				xtype: 'combocolumn',
+				store: 'app.store.RequestBudgets.channel_NRC'
+			},
+			{
+				width : 150,
+				header : 'Контракт с поставщиком',
+				dataIndex : 'tmside',
+				xtype: 'combocolumn',
+				store: 'app.store.RequestBudgets.tmside'
+			},
+			{
+				width: 100,
+				renderer: Ext.util.Format.numberRenderer('0.00'),
+				header: 'Сумма',
+				dataIndex: 'summ',
+				field: {
+     					xtype: 'numberfield',
+						minValue: 0,
+						decimalPrecision : 2,
+						decimalSeparator : ','
+
+				}
+			},
+			{
+				align: 'center',
+				width : 200,
+				header : 'Комментарий',
+				dataIndex : 'comm',
+				field: {
+					xtype: 'textfield'
+				}
+			},
+			{
+			header : 'Подтверждено',
+			xtype : 'checkcolumn',
+			dataIndex : 'priznak',
+			width : 100,
+			disabled: true
+		}
+
+
+
+						
+			
+
+			
+		]
+	}
+});
